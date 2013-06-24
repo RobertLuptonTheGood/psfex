0cd25cc4c55c22883c415c9210c25e3bb48bb901
Robert Lupton the Good <rhl@astro.princeton.edu>
2013-06-23 21:38:32 -0600

    There was no prototype for context_end

--- src/context.h
+++ src/context.h
@@ -54,6 +54,7 @@ typedef struct context
 contextstruct	*context_init(char **names, int *group, int ndim, int *degree,
 			int ngroup, int pcexflag);
 
+void context_end(contextstruct *context);
 
 #endif
 
